2b403acd07e30aca65f3cedfb1011bb9b677d3e4
Wesley de Moraes Lopes <wesmlopes@icloud.com>
2020-07-03 04:05:41 -0300

    frequencia de inimigos e criando a tela inicial

---
+++ scripts/load.js
@@ -0,0 +1,12 @@
+function preload() {
+    imagemCenario = loadImage('imagens/cenario/floresta.png');
+    imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+    imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+    imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
+    imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
+    imagemGameOver = loadImage('imagens/assets/game-over.png');
+    imagemTelaInicial = loadImage('imagens/assets/telaInicial.png');
+    fonteTelaInicial = loadFont('imagens/assets/fonteTelaInicial.otf');
+    somDoJogo = loadSound('sons/trilha_jogo.mp3');
+    somDoPulo = loadSound('sons/somPulo.mp3');
+}
